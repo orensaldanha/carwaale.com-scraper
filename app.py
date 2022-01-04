fd5d162a342d2509abceae6e6d62c1eb77b3c4c5
Samriddhi Shetty <87483055+SamriddhiShetty@users.noreply.github.com>
2022-01-04 21:36:52 +0530

    Backend-code

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import psycopg2
+
+dbcon = psycopg2.connect(
+        user='rwozksuc', 
+        password='3nv4b-4aaJb5bx0--2hIAZeoYVXateTm',
+        database='rwozksuc', 
+        host='john.db.elephantsql.com')
+app = Flask(__name__)
+
+@app.route("/")
+def form():
+    return "<p>form page</p>"
+
+@app.route("/results")
+def results():
+    cur = dbcon.cursor()
+
+    SQL = """ 
+		SELECT *
+		FROM cars
+		WHERE manual=%s AND automatic=%s"""
+    params = (manual, automatic)
+    cur.execute(SQL, params)
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
