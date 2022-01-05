3d22abb77b831ff5764ab452edbaeaee1efff53e
orensaldanha <36854536+orensaldanha@users.noreply.github.com>
2022-01-05 19:36:44 +0530

    added submit form

--- app.py
+++ app.py
@@ -1,37 +1,77 @@
 from flask import Flask, render_template, request
-# import psycopg2
+import psycopg2
 
-# dbcon = psycopg2.connect(
-#         user='rwozksuc',
-#         password='3nv4b-4aaJb5bx0--2hIAZeoYVXateTm',
-#         database='rwozksuc',
-#         host='john.db.elephantsql.com')
+dbcon = psycopg2.connect(
+        user='rwozksuc',
+        password='3nv4b-4aaJb5bx0--2hIAZeoYVXateTm',
+        database='rwozksuc',
+        host='john.db.elephantsql.com')
+
 app = Flask(__name__)
-
 
 @app.route("/")
 def form():
-    return render_template('submit.html')
-
+    return render_template('Car_search.html')
 
 @app.route("/results", methods=['GET', 'POST'])
 def results():
-    if request.method == "POST":
+    if request.method == 'POST':
+        print(request.form)
+        price_starting = float(request.form.get('price_starting'))
+        price_topend = float(request.form.get('price_topend'))
 
-        result = request.form
+        manual = request.form.get('manual')
+        if manual:
+            manual = True
 
-        return render_template("result.html", result=result)
+        automatic = request.form.get('automatic')
+        if automatic:
+            automatic = True
 
-    # cur = dbcon.cursor()
+        petrol = request.form.get('petrol')
+        if petrol:
+            petrol = True
 
-    # SQL = """
-    # 	SELECT *
-    # 	FROM cars
-    # 	WHERE manual=%s AND automatic=%s"""
-    # params = (manual, automatic)
-    # cur.execute(SQL, params)
+        diesel = request.form.get('diesel')
+        if diesel:
+            diesel = True
 
+        cng = request.form.get('cng')
+        if cng:
+            cng = True
+
+        electric = request.form.get('electric')
+        if electric:
+            cng = electric
+
+        seating_capacity = int(request.form.get('seating_capacity'))
+
+        app.logger.info(price_starting, price_topend, manual, automatic, petrol, diesel, cng, electric, seating_capacity)
+
+        SQL = """
+            SELECT id, name, company, image, price_starting, price_topend, mileage_l, mileage_u, manual, automatic, petrol, diesel, cng, electric, seating_capacity
+            FROM cars
+            WHERE price_starting > %s 
+                AND price_topend < %s
+                AND manual = COALESCE(%s, manual)
+                AND automatic = COALESCE(%s, automatic)
+                AND petrol = COALESCE(%s, petrol)
+                AND diesel = COALESCE(%s, diesel)
+                AND cng = COALESCE(%s, cng)
+                AND electric = COALESCE(%s, electric)
+                AND seating_capacity = %s;
+                """
+
+        params = (price_starting, price_topend, manual, automatic, petrol, diesel, cng, electric, seating_capacity)
+
+        cur = dbcon.cursor()
+
+        cur.execute(SQL, params)
+
+        cars = cur.fetchall()
+
+        return render_template("result.html", result=cars)
 
 if __name__ == "__main__":
-
     app.run(debug=True)
+    dbcon.close()
