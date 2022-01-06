a955f34fb3a76b626887faaac10a521eb882c03c
orensaldanha <36854536+orensaldanha@users.noreply.github.com>
2022-01-06 21:42:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
-from flask import send_file
+from flask import Flask, render_template, request, make_response, abort, send_file
+import psycopg2
+import psycopg2.extras
+import json
+from decimal import Decimal
 from docx import Document
 from io import BytesIO
-import psycopg2,logging
 
 dbcon = psycopg2.connect(
         user='rwozksuc',
@@ -12,25 +14,14 @@ dbcon = psycopg2.connect(
 
 app = Flask(__name__)
 
+def default(obj):
+    if isinstance(obj, Decimal):
+        return str(obj)
+    raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
+
 @app.route("/")
 def form():
     return render_template('Car_search.html')
-
-@app.route("/download")
-def download_doc():
-    
-    document = Document()
-    f = BytesIO()
-    # do staff with document
-    document.save(f)
-    f.seek(0)
-
-    return send_file(
-        f,
-        as_attachment=True,
-        attachment_filename='car_doc.docx'
-    )
-
 
 @app.route("/car/<int:id>")
 def car(id):
@@ -39,14 +30,17 @@ def car(id):
     cur.execute("SELECT * FROM cars WHERE id=%s", (id,))
 
     car = cur.fetchone()
+
+    cur.execute("SELECT * FROM company WHERE name=%s", (car[2],))
+
+    company = cur.fetchone()
+
     cur.close()
 
-    return render_template('Car.html', car=car)
-   
+    return render_template('Car.html', company=company, car=car)
 
 @app.route("/results", methods=['GET', 'POST'])
 def results():
-    app.logger.info(request.form)
     if request.method == 'POST':
         company = request.form.get('company')
 
@@ -106,11 +100,163 @@ def results():
 
         company = cur.fetchone()
 
-        app.logger.info(company)
         return render_template("Home.html", company=company,cars=cars)
 
+@app.route("/download/<int:id>")
+def download_doc(id):
+    
+    cur = dbcon.cursor()
+    cur.execute('SELECT * FROM cars WHERE id=%s', (id,))
+    car = cur.fetchone()
+    cur.close()
+
+    if car is None:
+        return '', 404
+
+    document = Document()
+    head =document.add_heading(car[1],0)
+    head.alignment = 1
+    document.add_paragraph(str(car[4]))
+    document.add_paragraph('Price range: '+str(car[5])+' lakh to '+str(car[6])+' lakh')
+    document.add_paragraph('Mileage: '+str(car[7])+' kmpl '+'-'+str(car[8])+' kmpl')
+    document.add_paragraph('Seating capacity: '+str(car[15]))
+    f = BytesIO()
+    document.save(f)
+    f.seek(0)
+
+    return send_file(
+        f,
+        as_attachment=True,
+        attachment_filename='car_doc.docx'
+    )
+
+#API routes
+@app.errorhandler(400)
+def not_found(error):
+    r = make_response(json.dumps({ 'error': 'Bad request' }), 400)
+    r.mimetype = 'application/json'
+    return r
+
+@app.errorhandler(404)
+def not_found(error):
+    r = make_response(json.dumps({'error': 'Not found'}), 404)
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars')
+def get_cars():
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars')
+    cars = cur.fetchall()
+    cur.close()
+
+    r = make_response(json.dumps({'cars': cars}, default=default))
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars', methods = ['POST'])
+def create_car():
+    if not request.json:
+        abort(400)
+    
+    for key in ['name' , 'company' , 'image' , 'summary' , 'price_starting' , 'price_topend' , 'mileage_l' , 'mileage_u' , 'manual' , 'automatic' , 'petrol' , 'diesel' , 'cng' , 'electric' , 'seating']:
+        if key not in request.json:
+            abort(400)
+
+    car = request.json
+
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+
+    SQL = '''INSERT INTO cars(name, company, image, summary, price_starting, price_topend, mileage_l, mileage_u, manual, automatic, petrol, diesel, cng, electric, seating_capacity)
+            VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id'''
+    
+    params = (car['name'], car['company'], car['image'], car['summary'], car['price_starting'], car['price_topend'], car['mileage_l'], car['mileage_u'], car['manual'], car['automatic'], car['petrol'], car['diesel'], car['cng'], car['electric'], car['seating'])
+
+    cur.execute(SQL, params)
+
+    id = cur.fetchone()['id']
+    cur.execute('SELECT * FROM cars WHERE id=%s', (id,))
+    car = cur.fetchone()
+    cur.close()
+
+    dbcon.commit()
+    
+    r = make_response(json.dumps(car, default=default), 201)
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars/<int:id>')
+def get_car(id):
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars where id=%s', (id,))
+    car = cur.fetchone()
+    cur.close()
+
+    if car is None:
+        abort(404)
+
+    r = make_response(json.dumps(car, default=default))
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars/<int:id>', methods = ['DELETE'])
+def delete_car(id):
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars where id=%s', (id,))
+    car = cur.fetchone()
+
+    if car is None:
+        abort(404)
+
+    cur.execute('delete from cars where id=%s', (id,))
+    cur.close()
+
+    return '', 204
+
+@app.route('/api/cars/<int:id>', methods = ['PUT'])
+def update_car(id):
+    if not request.json:
+        abort(400)
+    
+    for key in ['name' , 'company' , 'image' , 'summary' , 'price_starting' , 'price_topend' , 'mileage_l' , 'mileage_u' , 'manual' , 'automatic' , 'petrol' , 'diesel' , 'cng' , 'electric' , 'seating']:
+        if key not in request.json:
+            abort(400)
+
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars where id=%s', (id,))
+    car = cur.fetchone()
+
+    if car is None:
+        abort(404)
+
+    car = request.json
+
+    SQL = '''
+        UPDATE cars 
+            SET name=%s,
+                company=%s, 
+                image=%s, 
+                summary=%s, 
+                price_starting=%s, 
+                price_topend=%s, 
+                mileage_l=%s, 
+                mileage_u=%s, 
+                manual=%s, 
+                automatic=%s, 
+                petrol=%s, 
+                diesel=%s,
+                cng=%s, 
+                electric=%s, 
+                seating_capacity=%s
+            WHERE id=%s
+    '''
+    params = (car['name'], car['company'], car['image'], car['summary'], car['price_starting'], car['price_topend'], car['mileage_l'], car['mileage_u'], car['manual'], car['automatic'], car['petrol'], car['diesel'], car['cng'], car['electric'], car['seating'], id)
+
+    cur.execute(SQL, params)
+    cur.close()
+
+    return '', 204
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
     dbcon.close()
-
-
