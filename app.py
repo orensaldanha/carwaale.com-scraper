5808126bbb3c973f6fa3916763a072c30e6a98cd
orensaldanha <36854536+orensaldanha@users.noreply.github.com>
2022-01-06 19:09:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, abort
 import psycopg2
+import psycopg2.extras
+import json
+from decimal import Decimal
 
 dbcon = psycopg2.connect(
         user='rwozksuc',
@@ -8,6 +11,11 @@ dbcon = psycopg2.connect(
         host='john.db.elephantsql.com')
 
 app = Flask(__name__)
+
+def default(obj):
+    if isinstance(obj, Decimal):
+        return str(obj)
+    raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
 
 @app.route("/")
 def form():
@@ -87,6 +95,133 @@ def results():
         company = cur.fetchone()
 
         return render_template("Home.html", company=company,cars=cars)
+
+#API routes
+@app.errorhandler(400)
+def not_found(error):
+    r = make_response(json.dumps({ 'error': 'Bad request' }), 400)
+    r.mimetype = 'application/json'
+    return r
+
+@app.errorhandler(404)
+def not_found(error):
+    r = make_response(json.dumps({'error': 'Not found'}), 404)
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars')
+def get_cars():
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars')
+    cars = cur.fetchall()
+    cur.close()
+
+    r = make_response(json.dumps({'cars': cars}, default=default))
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars', methods = ['POST'])
+def create_car():
+    if not request.json:
+        abort(400)
+    
+    for key in ['name' , 'company' , 'image' , 'summary' , 'price_starting' , 'price_topend' , 'mileage_l' , 'mileage_u' , 'manual' , 'automatic' , 'petrol' , 'diesel' , 'cng' , 'electric' , 'seating']:
+        if key not in request.json:
+            abort(400)
+
+    car = request.json
+
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+
+    SQL = '''INSERT INTO cars(name, company, image, summary, price_starting, price_topend, mileage_l, mileage_u, manual, automatic, petrol, diesel, cng, electric, seating_capacity)
+            VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING id'''
+    
+    params = (car['name'], car['company'], car['image'], car['summary'], car['price_starting'], car['price_topend'], car['mileage_l'], car['mileage_u'], car['manual'], car['automatic'], car['petrol'], car['diesel'], car['cng'], car['electric'], car['seating'])
+
+    cur.execute(SQL, params)
+
+    id = cur.fetchone()['id']
+    cur.execute('SELECT * FROM cars WHERE id=%s', (id,))
+    car = cur.fetchone()
+    cur.close()
+
+    dbcon.commit()
+    
+    r = make_response(json.dumps(car, default=default), 201)
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars/<int:id>')
+def get_car(id):
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars where id=%s', (id,))
+    car = cur.fetchone()
+    cur.close()
+
+    if car is None:
+        abort(404)
+
+    r = make_response(json.dumps(car, default=default))
+    r.mimetype = 'application/json'
+    return r
+
+@app.route('/api/cars/<int:id>', methods = ['DELETE'])
+def delete_car(id):
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars where id=%s', (id,))
+    car = cur.fetchone()
+
+    if car is None:
+        abort(404)
+
+    cur.execute('delete from cars where id=%s', (id,))
+    cur.close()
+
+    return '', 204
+
+@app.route('/api/cars/<int:id>', methods = ['PUT'])
+def update_car(id):
+    if not request.json:
+        abort(400)
+    
+    for key in ['name' , 'company' , 'image' , 'summary' , 'price_starting' , 'price_topend' , 'mileage_l' , 'mileage_u' , 'manual' , 'automatic' , 'petrol' , 'diesel' , 'cng' , 'electric' , 'seating']:
+        if key not in request.json:
+            abort(400)
+
+    cur = dbcon.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+    cur.execute('select * from cars where id=%s', (id,))
+    car = cur.fetchone()
+
+    if car is None:
+        abort(404)
+
+    car = request.json
+
+    SQL = '''
+        UPDATE cars 
+            SET name=%s,
+                company=%s, 
+                image=%s, 
+                summary=%s, 
+                price_starting=%s, 
+                price_topend=%s, 
+                mileage_l=%s, 
+                mileage_u=%s, 
+                manual=%s, 
+                automatic=%s, 
+                petrol=%s, 
+                diesel=%s,
+                cng=%s, 
+                electric=%s, 
+                seating_capacity=%s
+            WHERE id=%s
+    '''
+    params = (car['name'], car['company'], car['image'], car['summary'], car['price_starting'], car['price_topend'], car['mileage_l'], car['mileage_u'], car['manual'], car['automatic'], car['petrol'], car['diesel'], car['cng'], car['electric'], car['seating'], id)
+
+    cur.execute(SQL, params)
+    cur.close()
+
+    return '', 204
 
 if __name__ == "__main__":
     app.run(debug=True)
